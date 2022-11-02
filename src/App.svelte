3bde7b66b8ce6a7770c0663d3e4ef904594ce62e
Otim <53155760+zztim888@users.noreply.github.com>
2022-11-02 15:19:27 +0100

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  let name = "world"
+  let src = './src/football.gif'
+</script>
+
+<h4>This is dynamic data</h4>
+<h1>hello {name}</h1>
+<h2>hello {name.toUpperCase()}</h2> <hr>
+
+<h4>Dynamic atributes</h4>
+<img src={src} alt="a football"> <br> <br>
+
+<img {src} alt="a football">
+
+<style>
+  img{
+    width: 200px;
+  }
+</style>
